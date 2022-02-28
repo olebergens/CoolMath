69976c033d37b6a891752745d9725506c417bf9d
Ole Bergens <infiziert.1337@gmail.com>
2022-03-01 00:14:26 +0100

    bubblesort description added

---
+++ src/main/kotlin/sorting/BubbleSort.kt
@@ -0,0 +1,56 @@
+package sorting
+
+import processing.core.PApplet
+
+class BubbleSort : PApplet() {
+
+    private var values: IntArray? = null
+    private var i = 0
+    private var start = false
+
+    companion object Factory {
+        fun run() {
+            val art = BubbleSort()
+            art.setSize(640, 480)
+            art.runSketch()
+        }
+    }
+
+    override fun setup() {
+        values = IntArray(width)
+        for (i in values!!.indices) values!![i] = random(height.toFloat()).toInt()
+    }
+
+    override fun draw() {
+        if (start) {
+            background(HSB)
+            if (i < values!!.size)
+                for (j in 0 until values!!.size - i - 1) {
+                    val a = values!![j]
+                    val b = values!![j + 1]
+                    if (a > b) swap(values!!, j, j + 1)
+                }
+            else noLoop()
+            i++
+            for (i in values!!.indices) {
+                stroke(128)
+                line(i.toFloat(), height.toFloat(), i.toFloat(), (height - values!![i]).toFloat())
+            }
+        }
+    }
+
+    override fun mouseClicked() {
+        start = !start
+    }
+
+    private fun swap(array: IntArray, a: Int, b: Int) {
+        val temp = array[a]
+        array[a] = array[b]
+        array[b] = temp
+    }
+
+}
+
+fun main() {
+    BubbleSort.run()
+}
---
+++ src/main/kotlin/sorting/QuickSort.kt
@@ -0,0 +1,4 @@
+package sorting
+
+class QuickSort {
+}
