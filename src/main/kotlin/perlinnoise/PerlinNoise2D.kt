860f0a906cad3111864a5b776503639b4f664fd9
Ole Bergens <infiziert.1337@gmail.com>
2022-03-01 16:16:29 +0100

    added perlin noise 2d fixed typo in readme added perlin noise 2d description and picture to readme

---
+++ src/main/kotlin/perlinnoise/PerlinNoise2D.kt
@@ -0,0 +1,42 @@
+package perlinnoise
+
+import processing.core.PApplet
+import kotlin.math.roundToInt
+
+class PerlinNoise2D : PApplet() {
+
+    private val increment = 0.02f
+
+    companion object Factory {
+        fun run() {
+            val art = PerlinNoise2D()
+            art.setSize(640, 480)
+            art.runSketch()
+        }
+    }
+
+    override fun setup() {
+        pixelDensity(1)
+    }
+
+    override fun draw() {
+        var yOffset = 0f
+        loadPixels()
+        for (y in 0 until height) {
+            var xOffset = 0f
+            for (x in 0 until width) {
+                val r: Float = noise(xOffset, yOffset)
+                pixels[x + y * width] = color(r * 255)
+                xOffset += increment
+            }
+            yOffset += increment
+        }
+        updatePixels()
+        noLoop()
+    }
+
+}
+
+fun main() {
+    PerlinNoise2D.run()
+}
